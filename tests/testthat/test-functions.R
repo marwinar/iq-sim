80c56b4410b7431030c0d8a0c311fbfb0abb7605
Marielle Winarto <mwinarto@gmail.com>
2024-09-29 00:19:35 +0200

    test naming

--- tests/testthat/test-functions.R
+++ tests/testthat/test-functions.R
@@ -1,4 +1,4 @@
-test_that("multiplication works", {
+test_that("simulating data works", {
   data <- simulate_data_with_reliability(100, .9, m = 100, sd = 15)
   expect_equal(dim(data), c(100, 3))
   expect_gt(mean(data$true_scores), 80)
